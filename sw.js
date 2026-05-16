bf1e29e232400155c64256dcd0955933d7eeb2d2
ValeComunales <valecomunales@gmail.com>
2026-05-16 15:02:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Mi Vida - Service Worker v3
+const CACHE_NAME = 'mi-vida-v3';
+
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.add('./index.html').catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const clone = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+      return response;
+    }).catch(() => {
+      return caches.match(event.request).then(cached => {
+        return cached || caches.match('./index.html');
+      });
+    })
+  );
+});
